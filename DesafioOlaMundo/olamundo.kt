323621a086666bd43b6a92dff415469165fe03fb
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-02-22 20:12:04 -0300

    first commit

---
+++ DesafioOlaMundo/olamundo.kt
@@ -0,0 +1,5 @@
+fun main(){
+    
+println("Meu primeiro aplicativo no Android feito pela IDE do Visaual Studio Code")
+
+}
---
+++ ExerciciosOlaMundo/OlaMundo.kt
@@ -0,0 +1,17 @@
+// PARA FAZER COMENTÁRIO DE LINHA
+
+/*
+COMENTÁRIO EM BLOCO
+
+ */
+
+ //Aplicativo quando compilado
+ // .apk - no Android
+ // .ipa - no iOS
+
+ //Aqui começamos definindo uma função
+ fun main(){
+
+    println("Olá, Mundo!")
+
+ }
