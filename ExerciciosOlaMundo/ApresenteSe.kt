ce21b0d9c69df7baac4e719ede40c5df89a85cdc
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-02-22 20:25:57 -0300

    Apresentacao Rodrigo

---
+++ ExerciciosOlaMundo/ApresenteSe.kt
@@ -0,0 +1,13 @@
+//Declarando varíaveis no Kotlin
+
+fun main(){
+
+    var idade = 19 // Idade
+    var altura = 1.79 //Altura 
+    var inicial = 'R' //Inicial do meu nome
+
+    //Imprimir as informações do professor interpolação de strings
+
+    println("Eu tenho ${idade} anos e ${altura} metros de altura e o meu nome começa com a letra ${inicial}")
+
+}
