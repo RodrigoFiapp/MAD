de56f4cd8b772c040210e3360f28fc38701f1f03
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-02-26 21:48:21 -0300

    Conceitos Basicos de Variaveis em kt

---
+++ Variaveis/variaveis.kt
@@ -0,0 +1,24 @@
+// Exemplo de variável em Kotlin
+
+//fun main () {
+
+ //   val nome = "Rodrigo" //Variável tipo string  // pode ser alterada multável
+ //   val idade = 19 //Variável do tipo inteira    //não pode ser alterada não multável
+//  println("Nome: $nome, Idade: $idade")
+//}
+
+//Declaração e Inicialização de Variáveis
+//Int, Double, String, boolean - Mais utilizados.
+
+fun main() {
+
+    val numeroInteiro: Int = 10
+    var numeroDecimal: Double = 3.14
+    val texto: String = "Olá Mundo!"
+    var status: Boolean = true // se true o stts é online se falso é offline(0 ou 1)
+
+    println("Número Inteiro: $numeroInteiro")
+    println("Número Decimal: $numeroDecimal")
+    println("Texto:$texto")
+    println("Status:$status")
+}
