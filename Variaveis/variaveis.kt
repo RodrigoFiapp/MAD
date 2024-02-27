0b180f852521b7f43bf2bec19f2bb0b64c4d4378
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-02-26 22:15:48 -0300

    update

--- Variaveis/variaveis.kt
+++ Variaveis/variaveis.kt
@@ -10,15 +10,51 @@
 //Declaração e Inicialização de Variáveis
 //Int, Double, String, boolean - Mais utilizados.
 
+// fun main() {
+
+//     val numeroInteiro: Int = 10
+//     var numeroDecimal: Double = 3.14
+//     val texto: String = "Olá Mundo!"
+//     var status: Boolean = true // se true o stts é online se falso é offline(0 ou 1)
+
+//     println("Número Inteiro: $numeroInteiro")
+//     println("Número Decimal: $numeroDecimal")
+//     println("Texto:$texto")
+//     println("Status:$status")
+// }
+
+//Varíaveis com tipo de dados
+//char, byte , short, Long
+
+// fun main() {
+
+//     val numeroInteiro: Int = 10
+//     val numeroLong: Long = 1000000
+//     val numeroDecimal: Double = 211.14
+//     val caractere: Char = 'V'
+//     val texto: String = "Kotlin também serve para back end"
+//     val verdadeiro: Boolean = true
+
+//     println("Inteiro: $numeroInteiro")
+//     println("Long: $numeroLong")
+//     println("Decimal:$numeroDecimal")
+//     println("caractere: $caractere")
+//     println("Texto: $texto")
+//     println("Verdadeiro: $verdadeiro")
+// }
+
+//Multabilidade e Imutabilidade no contexto de var e val
+
 fun main() {
 
-    val numeroInteiro: Int = 10
-    var numeroDecimal: Double = 3.14
-    val texto: String = "Olá Mundo!"
-    var status: Boolean = true // se true o stts é online se falso é offline(0 ou 1)
+    var mutavel: Int = 10 //mutavel
+    val imutavel: Int = 5 //imutavel
+    
 
-    println("Número Inteiro: $numeroInteiro")
-    println("Número Decimal: $numeroDecimal")
-    println("Texto:$texto")
-    println("Status:$status")
+    // mutavel = 15 //0k
+    // imutavel = 20 //Erro ! Não pode ser atribuida um valor a uma 
+    // //variável declarada como imutável
+    
+    println("Mutável: $mutavel")
+    println("Imutável: $imutavel")
 }
