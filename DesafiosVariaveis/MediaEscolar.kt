1a8805e31f9b24e80bfcb2af551ccecd6bbd2b61
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-02-27 15:59:48 -0300

    Desafio de Media Escolar

---
+++ DesafiosVariaveis/MediaEscolar.kt
@@ -0,0 +1,15 @@
+//Crie um programa para Calculadora de Média Escolar considere 3 notas. Considere: Nota1, Nota2,
+//Nota3 e média
+
+fun main () {
+val Nota1 : Double = 6.0
+val Nota2 : Double = 7.0
+val Nota3 : Double = 9.0
+
+val mediaEscolar = (Nota1 + Nota2 + Nota3) / 3
+
+println("A média das 3 notas deu : $mediaEscolar")
+
+}
+
+
