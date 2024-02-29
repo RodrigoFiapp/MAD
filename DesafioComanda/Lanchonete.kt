0f56c4356824c53d0860d1fa24f4fb88471799e8
Rodrigo Gonçalves Teixeira Filho <rodrigogoncalves2005t@gmail.com>
2024-02-28 22:55:44 -0300

    Desafio da Comanda incompleto

---
+++ DesafioComanda/Lanchonete.kt
@@ -0,0 +1,19 @@
+
+fun main () {
+    var quantidadeCoxinha = 3
+    var quantidadeCerveja = 2
+    var precoCoxinha  = 5.00
+    var precoCerveja = 15.00
+    var precoFinalCoxinha = 15.00
+    var precoFinalCerveja = 30.00
+    var soma = (precoCoxinha + precoCerveja)
+    var taxa = soma * 0.10
+    var total = (precoCoxinha + precoCerveja + taxa)
+
+println(" Console  ")
+// // // println("---------------------")
+// // // println(" => Lanchonete Rodrigo 2TDSPX <=")
+// // // println("--------------------------------------------")
+// // // println("$quantidadeCoxinha coxinhas      ")
+// // // println("$quantidadeCerveja cervejas      ")
+}
