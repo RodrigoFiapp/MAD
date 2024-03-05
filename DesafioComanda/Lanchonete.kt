3e10c42ba3fbc5d67e59ffcf9196ff17a402e93b
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-03-04 21:25:12 -0300

    Desafio da Comanda Finalizado

--- DesafioComanda/Lanchonete.kt
+++ DesafioComanda/Lanchonete.kt
@@ -1,19 +1,38 @@
+fun main() {
 
-fun main () {
-    var quantidadeCoxinha = 3
-    var quantidadeCerveja = 2
-    var precoCoxinha  = 5.00
-    var precoCerveja = 15.00
-    var precoFinalCoxinha = 15.00
-    var precoFinalCerveja = 30.00
-    var soma = (precoCoxinha + precoCerveja)
-    var taxa = soma * 0.10
-    var total = (precoCoxinha + precoCerveja + taxa)
+    //As variáveis precoCoxinha e precoCerveja armazenam os preços das coxinhas e cervejas, respectivamente.
+    var precoCoxinha = 3.50
+    var precoCerveja = 15.50
 
-println(" Console  ")
-// // // println("---------------------")
-// // // println(" => Lanchonete Rodrigo 2TDSPX <=")
-// // // println("--------------------------------------------")
-// // // println("$quantidadeCoxinha coxinhas      ")
-// // // println("$quantidadeCerveja cervejas      ")
+    //As variáveis qtdeCoxinha e qtdeCerveja armazenam as quantidades de coxinhas e cervejas, respectivamente.
+    var qtdeCoxinha = 3
+    var qtdeCerveja = 2
+
+    //totalCoxinha é calculado multiplicando a quantidade de  coxinhas pelo preço de uma coxinha.
+    var totalCoxinha = qtdeCoxinha * precoCoxinha
+
+    //totalCerveja é calculado multiplicando a quantidade de cervejas pelo preço de uma cerveja.    
+    var totalCerveja = qtdeCerveja * precoCerveja
+
+    //valorTotal é calculado somando os valores totais das coxinhas e cervejas.
+    var valorTotal = totalCoxinha + totalCerveja
+
+    //taxaServico é calculada como 10% do valorTotal.
+    var taxaServico = valorTotal * 0.10
+
+    //valorCobrado é calculado somando o valorTotal e a taxaServico.
+    var valorCobrado = valorTotal + taxaServico
+
+    println("\t .: Lanchonete Vinny :.")
+    println("---------------------------------")
+    println("$qtdeCoxinha coxinhas\t\t R$ %.2f".format(totalCoxinha))
+    println("$qtdeCerveja cervejas\t\t R$ %.2f".format(totalCerveja))
+    println("Somatória\t\t R$ %.2f".format(valorTotal))
+    println("---------------------------------")
+    println("Taxa 10%%\t\t R$ %.2f".format(taxaServico))
+    println("---------------------------------")
+    println("Conta Final\t\t R$ %.2f".format(valorCobrado))
+
+    //Lembre-se de que em Kotlin, a impressão é feita usando println() e a formatação é realizada usando format().
+
 }
