a60fb81a7c637f7d83b25cc7f58f1c8a6e772643
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-03-06 17:06:49 -0300

    Desafio imc incompleto

---
+++ DesafioIMC/imc.kt
@@ -0,0 +1,24 @@
+// fun main () {
+//     val pesoUsuario : Double = 54.0
+//     val alturaUsuario : Double = 1.77
+
+//     val imc : Double = pesoUsuario / (alturaUsuario * alturaUsuario)
+
+//     println("O peso é : $pesoUsuario")
+//     println("A altura é : $alturaUsuario")
+//     println("O IMC do usuário é : %.2f".format(imc))
+// }
+
+fun main() {
+    print("Digite seu peso: ")
+    var pesoInput = readLine()
+
+    
+
+    print("Digite sua altura: ")
+    var alturaInput = readLine()
+
+
+    // val peso = pesoInput.toIntOrNull()
+    // val altura = alturaInput.toIntOrNull()
+}
